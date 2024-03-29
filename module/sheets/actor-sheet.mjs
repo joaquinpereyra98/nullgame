8a2c963a9a5ef41e8075f9f45d7f4ac70e0bfbbe
joaquinpereyra98 <24190917+joaquinpereyra98@users.noreply.github.com>
2024-03-29 20:05:19 -0300

    feat: basic actor sheet

---
+++ module/sheets/actor-sheet.mjs
@@ -0,0 +1,81 @@
+/**
+   * An Actor sheet for player character type actors.
+   * @extends {ActorSheet}
+   */
+export class NullGameActorSheet extends ActorSheet {
+  /** @override */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ['null-game', 'sheet', 'actor'],
+      width: 600,
+      height: 600,
+      tabs: [
+        {
+          navSelector: '.sheet-tabs',
+          contentSelector: '.sheet-body',
+          initial: 'features',
+        },
+      ],
+    });
+  }
+
+  /** @override */
+  get template() {
+    return `systems/null-game/templates/actor/actor-${this.actor.type}-sheet.hbs`;
+  }
+
+  /** @override */
+  getData() {
+    const context = super.getData();
+    const actorData = context.data;
+    context.system = actorData.system;
+    context.flags = actorData.flags;
+    if (actorData.type == 'character') {
+      this._prepareItems(context);
+      this._prepareCharacterData(context);
+    }
+    context.rollData = context.actor.getRollData();
+
+    return context;
+  }
+
+  /**
+   * Organize and classify Items for Character sheets.
+   *
+   * @param {Object} actorData The actor to prepare.
+   *
+   * @return {undefined}
+   */
+  _prepareCharacterData(context) {
+  }
+
+  /**
+   * Organize and classify Items for Character sheets.
+   *
+   * @param {Object} actorData The actor to prepare
+   * @return {undefined}
+   */
+  _prepareItems(context) {
+    const skills = [];
+    const features = [];
+
+    for (let i of context.items) {
+      i.img = i.img || Item.DEFAULT_ICON;
+      if (i.type === 'item') {
+        skills.push(i);
+      }
+      else if (i.type === 'feature') {
+        features.push(i);
+      }
+    }
+    context.skills = skills;
+    context.features = features;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+  }
+}
