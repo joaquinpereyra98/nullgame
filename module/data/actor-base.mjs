324f7a343f297616dfbe359581ab6fd5c563096a
joaquinpereyra98 <24190917+joaquinpereyra98@users.noreply.github.com>
2024-03-29 20:06:14 -0300

    feat: basic data model actor

---
+++ module/data/actor-base.mjs
@@ -0,0 +1,46 @@
+export default class NullGameActor extends foundry.abstract.TypeDataModel {
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    const requiredInteger = { required: true, nullable: false, integer: true };
+    const requiredStringer = { required: true, blank: true };
+    return {
+      bars: new fields.SchemaField({
+        firstBar: new fields.SchemaField({
+          label: new fields.StringField({ ...requiredStringer }),
+          value: new fields.NumberField({
+            ...requiredInteger,
+            initial: 10,
+            min: 0,
+          }),
+          max: new fields.NumberField({ ...requiredInteger, initial: 10 }),
+        }),
+        secondBar: new fields.SchemaField({
+          label: new fields.StringField({ ...requiredStringer }),
+          value: new fields.NumberField({
+            ...requiredInteger,
+            initial: 10,
+            min: 0,
+          }),
+          max: new fields.NumberField({ ...requiredInteger, initial: 10 }),
+        }),
+        thirdBar: new fields.SchemaField({
+          label: new fields.StringField({ ...requiredStringer }),
+          value: new fields.NumberField({
+            ...requiredInteger,
+            initial: 10,
+            min: 0,
+          }),
+          max: new fields.NumberField({ ...requiredInteger, initial: 10 }),
+        }),
+      }),
+      biography: new fields.HTMLField(...requiredStringer),
+      textBoxs: new fields.SchemaField({
+        firstTextBox: new fields.StringField({ ...requiredStringer }),
+        secondTextBox: new fields.StringField({ ...requiredStringer }),
+        thirdTextBox: new fields.StringField({ ...requiredStringer }),
+      }),
+      level: new fields.NumberField({...requiredStringer, min: 0}),
+      size: new fields.StringField({...requiredStringer})
+    };
+  }
+}
